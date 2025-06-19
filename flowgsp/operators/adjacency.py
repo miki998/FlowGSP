[MOD] - fix normalization of adjacency matrix issue

flowgsp/operators/adjacency.py
@@ -2,7 +2,7 @@
 Copyright © 2025 Chun Hei Michael Chan, MIPLab EPFL
 """
 
-from flowgsp.utils import np, hermitian, TV
+from ..utils import np, hermitian, TV
 from .base import Operator
 from .jordan_destroy import destroy_jordan_blocks, destroy_zero_eigenvals
 from typing import Optional
@@ -21,7 +21,7 @@ class Adjacency(Operator):
         if normalize is not None:
             if normalize not in ['right', 'left', 'symmetric']:
                 raise ValueError("normalize must be one of ['right', 'left', 'symmetric']")
-            self.normalize_operator(order=normalize)
+            self.params['normalize'] = normalize
         self.compute_basis(in_degree=in_degree, decomposition=decomposition)
 
     def compute_basis(self, in_degree:bool=True, decomposition:str='eig'):
@@ -29,6 +29,7 @@ class Adjacency(Operator):
         Compute the basis for the Laplacian operator.
         The basis is computed as the eigenvectors of the Laplacian matrix.
         """
+        self.graph.adj_matrix = self.normalize_operator(self.graph.adj_matrix, order=self.params['normalize'])
         self.M = self.graph.adj_matrix
         if self.is_symmetric():
             self.V, self.U = np.linalg.eigh(self.M)

flowgsp/operators/base.py
@@ -2,7 +2,7 @@
 Copyright © 2025 Chun Hei Michael Chan, MIPLab EPFL
 """
 
-from flowgsp.utils import np, hermitian
+from ..utils import np, hermitian
 
 class Operator:
     """
@@ -80,43 +80,45 @@ class Operator:
         ret = self.U @ coef
         return ret
 
-    def normalize_operator(self, order="left"):
+    def normalize_operator(self, A:np.ndarray, order:str="left"):
         """
         Normalize the operator matrix by in-degrees / out-degrees / symmetric
 
         Parameters
         ----------
+        A : numpy.ndarray
+            The operator matrix to be normalized.
         order : str
             The normalization method. Can be "right", "left", or "symmetric".
 
         Returns
         -------
-        normA : np.ndarray
-            The normalized adjacency matrix
+        normA : numpy.ndarray
+            The normalized operator matrix.
         """
-        if np.any(self.M.imag != 0):
+        if np.any(A.imag != 0):
             raise ValueError("Complex values in adjacency matrix")
 
         if order == "right":
-            outdegrees = np.sum(self.M, axis=0)
+            outdegrees = np.sum(A, axis=0)
             factors_in = np.diag(np.divide(1, outdegrees, where=np.abs(outdegrees) > 1e-10))
-            normA = self.M @ factors_in
+            normA = A @ factors_in
 
         if order == "left":
-            indegrees = np.sum(self.M, axis=1)
+            indegrees = np.sum(A, axis=1)
             factors_out = np.diag(np.divide(1, indegrees, where=np.abs(indegrees) > 1e-10))
-            normA = factors_out @ self.M
+            normA = factors_out @ A
 
         if order == "symmetric":
-            indegrees = np.sum(self.M, axis=1)
-            outdegrees = np.sum(self.M, axis=0)
+            indegrees = np.sum(A, axis=1)
+            outdegrees = np.sum(A, axis=0)
 
             indegrees = np.sqrt(np.abs(indegrees))
             outdegrees = np.sqrt(np.abs(outdegrees))
 
             factors_in = np.diag(np.divide(1, indegrees, where=np.abs(indegrees) > 1e-10))
             factors_out = np.diag(np.divide(1, outdegrees, where=np.abs(outdegrees) > 1e-10))
-            normA = factors_out @ self.M @ factors_in
+            normA = factors_out @ A @ factors_in
 
         self.params["normalization"] = order
 

flowgsp/operators/laplacian.py
@@ -2,7 +2,7 @@
 Copyright © 2025 Chun Hei Michael Chan, MIPLab EPFL
 """
 
-from flowgsp.utils import np, hermitian
+from ..utils import np, hermitian
 from .base import Operator
 from .jordan_destroy import destroy_jordan_blocks_laplacian
 from typing import Optional
@@ -20,7 +20,7 @@ class Laplacian(Operator):
         if normalize is not None:
             if normalize not in ['right', 'left', 'symmetric']:
                 raise ValueError("normalize must be one of ['right', 'left', 'symmetric']")
-            self.normalize_operator(order=normalize)
+            self.params['normalize'] = normalize
         self.compute_basis(in_degree=in_degree)
 
     def compute_basis(self, in_degree:bool=True):
@@ -28,7 +28,9 @@ class Laplacian(Operator):
         Compute the basis for the Laplacian operator.
         The basis is computed as the eigenvectors of the Laplacian matrix.
         """
+        self.graph.adj_matrix = self.normalize_operator(self.graph.adj_matrix, order=self.params['normalize'])
         self.M = self.compute_directed_laplacian(self.graph.adj_matrix, in_degree=in_degree)
+        
         if self.is_symmetric():
             self.V, self.U = np.linalg.eig(self.M)
         else: