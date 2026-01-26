02bd0ed8b9966f4536ca6ba8d85498af9e249cd0
Michael 米高 <miki998Chan@gmail.com>
2026-01-26 13:59:36 +0100

    fix(test): Rematch library loading

--- flowgsp/__init__.py
+++ flowgsp/__init__.py
@@ -2,4 +2,16 @@
 Copyright © 2025 Chun Hei Michael Chan, MIPLab EPFL
 """
 
-from .stats_utils import *  # noqa: F401, F403
+from . import graphs  # noqa: F401, F403
+from . import filters  # noqa: F401, F403
+from . import learning  # noqa: F401, F403
+from . import utils  # noqa: F401, F403
+from . import sampling  # noqa: F401, F403
+from . import surrogates  # noqa: F401, F403
+from . import source_separation  # noqa: F401, F403
+from . import operators  # noqa: F401, F403
+
+from .constants import *  # noqa: F401, F403
+
+__version__ = "0.0.1"
+__release_date__ = "2025-12-24"
