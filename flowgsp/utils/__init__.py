7af9b8fe6dea06fb2cdcd9949adddbe10cebdcc7
Michael 米高 <miki998Chan@gmail.com>
2026-01-26 14:50:26 +0100

    fix: Requirements.txt typo and loading lib

--- flowgsp/utils/__init__.py
+++ flowgsp/utils/__init__.py
@@ -7,6 +7,14 @@ from .metrics import *
 from .numericals import *
 from .plot_utils import *
 from ..stats.stats_utils import *  # noqa: F401, F403
+from .logging_config import (  # noqa: F401, E402
+    setup_logger,
+    get_logger,
+    configure_experiment_logging,
+    set_library_log_levels,
+    disable_logging,
+    enable_logging,
+)
 
 __version__ = '0.0.1'
 __release_date__ = '2025-10-16'
